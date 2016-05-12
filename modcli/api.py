2df0c6d50939f79630ce5a58d04abab81630f447
Alexandre Cunha <alexandre.cunha@gmail.com>
2016-05-12 08:19:11 -0500

    - created new api commands to register and init user - support tokens created with a password

---
+++ modcli/api.py
@@ -0,0 +1,86 @@
+import argparse
+import json
+import os
+import sys
+from urllib import request
+from urllib.error import HTTPError
+
+from modcli import settings, token
+
+_MOD_HOME = os.path.expanduser('~/.mod')
+
+
+def post(url_path: str, data: dict):
+    url = '{0}{1}'.format(settings.API_BASE_URL, url_path)
+    req = request.urlopen(url, json.dumps(data).encode())
+    result = req.read()
+    if result:
+        return json.loads(result.decode())
+    return result
+
+
+def register(args):
+    data = {
+        'user_id': args.user_id,
+        'name': args.name,
+        'email': args.email,
+        'password': args.password,
+    }
+    post('/users', data)
+    print('User {0} registered'.format(args.user_id))
+    init(args.user_id, args.password)
+
+
+def init(user_id, password):
+    access_token = token.get_token_password(user_id, password)
+    os.makedirs(_MOD_HOME, exist_ok=True)
+    with open(os.path.join(_MOD_HOME, 'config'), 'w') as f:
+        f.write(json.dumps({'user_id': user_id}, sort_keys=True, indent=4))
+        f.write(os.linesep)
+    with open(os.path.join(_MOD_HOME, 'access_token'), 'w') as f:
+        f.write(access_token)
+        f.write(os.linesep)
+
+
+def _init(args):
+    init(args.user_id, args.password)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Access MOD API')
+    subparsers = parser.add_subparsers()
+    # register
+    parser_register = subparsers.add_parser('register', help='Register a new user')
+    parser_register.add_argument('user_id', help='User identification (length: >5)')
+    parser_register.add_argument('password', help='Password')
+    parser_register.add_argument('name', help='Full name')
+    parser_register.add_argument('email', help='User email address')
+    parser_register.set_defaults(func=register)
+    # init
+    parser_init = subparsers.add_parser('init', help='Obtain a token and save')
+    parser_init.add_argument('user_id', help='User identification')
+    parser_init.add_argument('password', help='Password')
+    parser_init.set_defaults(func=_init)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        exit(1)
+    args = parser.parse_args()
+    if hasattr(args, 'func'):
+        try:
+            args.func(args)
+            exit(0)
+        except HTTPError as e:
+            print(e)
+            resp = e.read()
+            if resp:
+                print(resp.decode())
+            exit(1)
+        except Exception as e:
+            print(e)
+            exit(1)
+    else:
+        parser.print_usage()
+
+if __name__ == '__main__':
+    main()
--- modcli/token.py
+++ modcli/token.py
@@ -2,7 +2,7 @@ import argparse
 import base64
 import json
 import os
-import logging
+import sys
 from urllib import request
 
 from modcli import settings, crypt, get_agent_info
@@ -22,19 +22,22 @@ def decode_and_decrypt(private_key: str, data: str):
     return crypt.decrypt(device_key_text, encrypted)
 
 
-def get_token(args):
+def get_token(user_data: dict):
     req = request.urlopen('{0}/users/nonce'.format(settings.API_BASE_URL))
     nonce = json.loads(req.read().decode())['nonce']
     data = {
         'nonce': nonce,
-        'user_id': args.user_id,
         'agent': get_agent_info(),
     }
+    data.update(user_data)
     message = json.dumps({'message': encrypt_and_encode(settings.API_KEY_PATH, json.dumps(data)).decode()})
     req = request.urlopen('{0}/users/tokens'.format(settings.API_BASE_URL), message.encode())
     token_message = req.read().decode()
-    token = decode_and_decrypt(os.path.expanduser(args.ssh_key_file), json.loads(token_message)['message'])
-    return token
+    return json.loads(token_message)['message']
+
+
+def get_token_password(user_id, password):
+    return get_token({'user_id': user_id, 'password': password})
 
 
 def main():
@@ -45,13 +48,20 @@ def main():
     parser.add_argument('--ssh-key-file', help='A ssh key .pem file (default: ~/.ssh/id_rsa)')
     parser.add_argument('user_id', help='The user id')
 
-    args = parser.parse_args()
-    if args.password:
-        logging.error('argument --password is not yet supported')
+    if len(sys.argv) == 1:
+        parser.print_help()
         exit(1)
-    if args.ssh_key and not args.ssh_key_file:
-        args.ssh_key_file = '~/.ssh/id_rsa'
-    print(get_token(args))
+    args = parser.parse_args()
+
+    if args.password:
+        token = get_token_password(args.user_id, args.password)
+    else:
+        if args.ssh_key and not args.ssh_key_file:
+            args.ssh_key_file = '~/.ssh/id_rsa'
+        token = decode_and_decrypt(os.path.expanduser(args.ssh_key_file), get_token({'user_id': args.user_id}))
+
+    print(token)
+    exit(0)
 
 
 if __name__ == '__main__':
