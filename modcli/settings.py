ded2edc5caab24ffe97c0f23a1a18b60d6a70b4d
Alex Cunha <alexandre.cunha@gmail.com>
2017-09-28 10:07:40 -0500

    Updated DEV cloud endpoints to HTTPS

--- modcli/settings.py
+++ modcli/settings.py
@@ -3,5 +3,5 @@ import os
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 API_BASE_URL = os.environ.get('MOD_API_BASE_URL', 'https://api.moddevices.com/v2/').rstrip('/')
-DEV_API_BASE_URL = os.environ.get('MOD_DEV_API_BASE_URL', 'http://api.dev.moddevices.com/v2/').rstrip('/')
+DEV_API_BASE_URL = os.environ.get('MOD_DEV_API_BASE_URL', 'https://api-dev.moddevices.com/v2/').rstrip('/')
 API_KEY_PATH = os.path.join(_ROOT, 'mod_api_key.pub')
