352cb7e9bd1ba3b4d25888b0b246ce1cf0c4f875
Alex Cunha <alexandre.cunha@gmail.com>
2017-10-11 15:07:35 -0500

    Use HTTPS in dependency links on setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'pycrypto==2.7a1',
     ],
     dependency_links=[
-        'http://github.com/dlitz/pycrypto/tarball/v2.7a1#egg=pycrypto-2.7a1',
+        'https://github.com/dlitz/pycrypto/tarball/v2.7a1#egg=pycrypto-2.7a1',
     ],
     entry_points={
         'console_scripts': [
