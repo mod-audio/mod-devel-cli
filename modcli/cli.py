ae96a6ec7c169d732df2245c70942cded0f3c2f8
Alex Cunha <ale@rcunha.net>
2018-05-10 12:12:24 -0500

    Added `--one-time` option
    * Using `--one-time` will simply print the token without storing it

--- modcli/cli.py
+++ modcli/cli.py
@@ -24,7 +24,8 @@ def auth():
 @click.option('-u', '--username', type=str, prompt=True, help='User ID')
 @click.option('-p', '--password', type=str, prompt=True, hide_input=True, help='User password')
 @click.option('-s', '--show_token', type=bool, help='Print the JWT token obtained', is_flag=True)
-def login(username: str, password: str, show_token: bool):
+@click.option('-o', '--one_time', type=bool, help='Only print token once (do not store it)', is_flag=True)
+def login(username: str, password: str, show_token: bool, one_time: bool):
     result = requests.post(_api_url('/users/tokens'), json={
         'user_id': username,
         'password': password,
@@ -34,8 +35,11 @@ def login(username: str, password: str, show_token: bool):
         click.echo(crayons.red('Error: {0}'.format(result.json()['error-message'])), err=True)
         exit(1)
     token = result.json()['message'].strip()
-    config.save_token(token, username)
-    if show_token:
+
+    if not one_time:
+        config.save_token(token, username)
+
+    if show_token or one_time:
         print(token.strip())
     else:
         click.echo(crayons.green('You\'re now logged in as [{0}].'.format(username)))
