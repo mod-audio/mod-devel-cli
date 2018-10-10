ea6636a50913089989fb5a2f08f5ef0fc141a50b
Alex Cunha <ale@rcunha.net>
2018-10-09 23:11:49 -0500

    Properly support authenticating in multiple environments

--- modcli/cli.py
+++ modcli/cli.py
@@ -23,14 +23,39 @@ def auth():
     pass
 
 
+@click.command(help='Authenticate user with SSO (MOD Forum)')
+@click.option('-s', '--show-token', type=bool, help='Print the JWT token obtained', is_flag=True)
+@click.option('-o', '--one-time', type=bool, help='Only print token once (do not store it)', is_flag=True)
+@click.option('-e', '--environment', type=click.Choice(['prod', 'labs', 'dev']), default='prod', help='MOD API Environment')
+def login_sso(show_token: bool, one_time: bool, environment: str):
+    api_url = settings.API_URLS[environment]
+    result = requests.post(get_url(api_url, '/users/tokens_sso'), json={
+        'agent': 'modcli:{0}'.format(__version__),
+    })
+    if result.status_code != 200:
+        click.echo(crayons.red('Error: {0}'.format(result.json()['error-message'])), err=True)
+        exit(1)
+    token = result.json()['message'].strip()
+    _, payload, _ = token.split('.')
+    username = payload['user_id']
+
+    if not one_time:
+        config.save_token(token, username, api_url, environment)
+
+    if show_token or one_time:
+        print(token.strip())
+    else:
+        click.echo(crayons.green('You\'re now logged in as [{0}].'.format(username)))
+
+
 @click.command(help='Authenticate user')
 @click.option('-u', '--username', type=str, prompt=True, help='User ID')
 @click.option('-p', '--password', type=str, prompt=True, hide_input=True, help='User password')
 @click.option('-s', '--show-token', type=bool, help='Print the JWT token obtained', is_flag=True)
 @click.option('-o', '--one-time', type=bool, help='Only print token once (do not store it)', is_flag=True)
-@click.option('--api-url', type=str, help='MOD API Url (i.e. https://api.moddevices.com/v2/')
-def login(username: str, password: str, show_token: bool, one_time: bool, api_url: str):
-    api_url = api_url or settings.API_URL
+@click.option('-e', '--environment', type=click.Choice(['prod', 'labs', 'dev']), default='prod', help='MOD API Environment')
+def login(username: str, password: str, show_token: bool, one_time: bool, environment: str):
+    api_url = settings.API_URLS[environment]
     result = requests.post(get_url(api_url, '/users/tokens'), json={
         'user_id': username,
         'password': password,
@@ -42,7 +67,7 @@ def login(username: str, password: str, show_token: bool, one_time: bool, api_ur
     token = result.json()['message'].strip()
 
     if not one_time:
-        config.save_token(token, username, api_url)
+        config.save_token(token, username, api_url, environment)
 
     if show_token or one_time:
         print(token.strip())
@@ -51,8 +76,9 @@ def login(username: str, password: str, show_token: bool, one_time: bool, api_ur
 
 
 @click.command(help='Show current active access JWT token')
-def active_token():
-    token = config.read_token()
+@click.option('-e', '--environment', type=click.Choice(['prod', 'labs', 'dev']), default=None, help='MOD API Environment')
+def active_token(environment: str):
+    token = config.read_token(environment)
     if not token:
         click.echo(crayons.red('You must authenticate first.'), err=True)
         click.echo('Try:\n $ modcli auth login')
--- modcli/config.py
+++ modcli/config.py
@@ -12,26 +12,44 @@ def _get_config_dir():
     return settings.CONFIG_DIR
 
 
-def save_token(token: str, username: str, api_url: str):
-    for path in [settings.TOKEN_PATH, settings.INFO_PATH]:
+def _remove_files(files):
+    for path in files:
         if os.path.isfile(path):
             os.remove(path)
-    with os.fdopen(os.open(settings.TOKEN_PATH, os.O_WRONLY | os.O_CREAT, stat.S_IRUSR | stat.S_IWUSR), 'w') as fh:
-        fh.write(token)
-        fh.write(os.linesep)
-    data = {
-        'token_path': settings.TOKEN_PATH,
-        'username': username,
-        'created': str(datetime.utcnow()),
-        'remote': api_url,
-    }
-    with os.fdopen(os.open(settings.INFO_PATH, os.O_WRONLY | os.O_CREAT, stat.S_IRUSR | stat.S_IWUSR), 'w') as fh:
-        fh.write(json.dumps(data))
-        fh.writelines(os.linesep)
 
 
-def read_token():
-    if not os.path.isfile(settings.TOKEN_PATH):
+def save_token(token: str, username: str, api_url: str, environment: str):
+    token_paths = [
+        os.path.join(settings.CONFIG_DIR, i)
+        for i in ['access_token', '{0}.access_token'.format(environment)]
+    ]
+    info_paths = [
+        os.path.join(settings.CONFIG_DIR, i)
+        for i in ['info.json', '{0}.info.json'.format(environment)]
+    ]
+    _remove_files(token_paths + info_paths)
+
+    for token_path in token_paths:
+        with os.fdopen(os.open(token_path, os.O_WRONLY | os.O_CREAT, stat.S_IRUSR | stat.S_IWUSR), 'w') as fh:
+            fh.write(token)
+            fh.write(os.linesep)
+    for info_path in info_paths:
+        data = {
+            'token_path': token_paths[1],
+            'username': username,
+            'created': str(datetime.utcnow()),
+            'remote': api_url,
+            'environment': environment,
+        }
+        with os.fdopen(os.open(info_path, os.O_WRONLY | os.O_CREAT, stat.S_IRUSR | stat.S_IWUSR), 'w') as fh:
+            fh.write(json.dumps(data))
+            fh.writelines(os.linesep)
+
+
+def read_token(environment: str):
+    token_filename = 'access_token' if environment is None else '{0}.access_token'.format(environment)
+    token_path = os.path.join(settings.CONFIG_DIR, token_filename)
+    if not os.path.isfile(token_path):
         return None
-    with open(settings.TOKEN_PATH, 'r') as fh:
+    with open(token_path, 'r') as fh:
         return fh.read().strip()
