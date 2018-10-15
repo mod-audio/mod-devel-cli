392780ed1bc5d2453688cf0cd53fee97f838258e
Ale Cunha <ale@rcunha.net>
2018-10-14 20:10:06 -0500

    Removed local environment (used for debugging purposes)

--- modcli/settings.py
+++ modcli/settings.py
@@ -3,6 +3,5 @@ import os
 CONFIG_DIR = os.path.expanduser('~/.config/modcli')
 API_URLS = {
     'labs': 'https://api-labs.moddevices.com/v2',
-    'local': 'http://localhost:8000/v2',
 }
 DEFAULT_ENV = 'labs'
