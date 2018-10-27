cc087112f3194fb1bd0f1edb6e8e5152aeef852a
Ale Cunha <ale@rcunha.net>
2018-10-27 00:46:11 -0500

    Instead of fixing in here it should be exported as the OS level

--- modcli/__init__.py
+++ modcli/__init__.py
@@ -1,10 +1,5 @@
-import os
 from modcli import config
 
 __version__ = '1.1.1'
-
-# http://click.pocoo.org/5/python3/
-os.environ['LC_ALL'] = 'C.UTF-8'
-os.environ['LANG'] = 'C.UTF-8'
 
 context = config.read_context()
